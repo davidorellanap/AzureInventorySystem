e81e26db11c560f85e1e79ed0c72ecbee0b296db
David Hernán Orellana Palacios <ia.dhpalacios@ufg.edu.sv>
2024-06-25 21:29:06 -0600

    Add models for payments and orders
    Introduced new models in the Inventory.Models namespace to enhance the inventory management system's capabilities. These include PaymentReceive, PaymentType, and PurchaseOrder models. The PaymentReceive model manages payment receipts with fields for payment details, including a default for full payments. The PaymentType model allows for the definition of various payment methods, marked by required payment type names. The PurchaseOrder model facilitates the management of purchase orders, incorporating comprehensive fields for order details, financial calculations, and vendor information. These additions lay the groundwork for improved financial transaction and order tracking within the system.

---
+++ Inventory.Models/PurchaseOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class PurchaseOrder
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BranchId { get; set; }
+        public int VendorId { get; set; }
+        public DateTimeOffset DateOfOrder { get; set; }
+        public DateTimeOffset DateOfDelivery { get; set; }
+        public int CurrencyId { get; set; }
+        public int PurchaseTypeId { get; set; }
+        public string Remarks { get; set; }
+        public double Amount { get; set; }
+        public double SubTotal { get; set; }
+        public double Discount { get; set; }
+        public double Tax { get; set; }
+        public double Total { get; set; }
+    }
+}
