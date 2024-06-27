cc9b21b4679282b48760a3223b085280fe416028
David Hernán Orellana Palacios <ia.dhpalacios@ufg.edu.sv>
2024-06-26 21:24:54 -0600

    Enhanced PurchaseOrder model and added classes
    - Added data annotations in PurchaseOrder.cs for property display names and validation.
    - Introduced PurchaseOrderLines collection property in PurchaseOrder.cs to link with PurchaseOrderLine items.
    - Created PurchaseOrderLine.cs with detailed properties including financial figures and relationships, all equipped with display names through data annotations.
    - Added a new PurchaseType.cs class with required properties and a `[Required]` annotation on `PurchaseTypeName` to ensure it's always provided.

--- Inventory.Models/PurchaseOrder.cs
+++ Inventory.Models/PurchaseOrder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,17 @@ namespace Inventory.Models
     public class PurchaseOrder
     {
         public int Id { get; set; }
+        [Display(Name = "Purchase Order")]
         public string Name { get; set; }
+        [Display(Name = "Branch")]
         public int BranchId { get; set; }
+        [Display(Name = "Vendor")]
         public int VendorId { get; set; }
         public DateTimeOffset DateOfOrder { get; set; }
         public DateTimeOffset DateOfDelivery { get; set; }
+        [Display(Name = "Currency")]
         public int CurrencyId { get; set; }
+        [Display(Name = "Purchase Type")]
         public int PurchaseTypeId { get; set; }
         public string Remarks { get; set; }
         public double Amount { get; set; }
@@ -22,5 +28,6 @@ namespace Inventory.Models
         public double Discount { get; set; }
         public double Tax { get; set; }
         public double Total { get; set; }
+        public ICollection<PurchaseOrderLine> PurchaseOrderLines { get; set; } = new List<PurchaseOrderLine>();
     }
 }
